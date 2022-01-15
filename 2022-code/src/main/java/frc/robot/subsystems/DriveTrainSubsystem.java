43f7a38dc69a29f7cbe4e1cc2aabb51239f7ba98
Neutrino Controls <NeutrinoControls@gmail.com>
2022-01-15 13:36:18 -0800

    Get Real

--- 2022-code/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ 2022-code/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -12,15 +12,15 @@ public class DriveTrainSubsystem extends SubsystemBase {
     private CANSparkMax m_rightMotor2 = new CANSparkMax(MotorCON.MOTOR_CONTROLLER_DRIVE_RIGHT_2, MotorType.kBrushless);
     private CANSparkMax m_leftMotor1 = new CANSparkMax(MotorCON.MOTOR_CONTROLLER_DRIVE_LEFT_1, MotorType.kBrushless);
     private CANSparkMax m_leftMotor2 = new CANSparkMax(MotorCON.MOTOR_CONTROLLER_DRIVE_LEFT_2, MotorType.kBrushless);
-    private MotorControllerGroup m_rightmotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
-    private MotorControllerGroup m_leftmotors = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
+    private MotorControllerGroup m_rightMotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
+    private MotorControllerGroup m_leftMotors = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
 
     private Joystick m_rightJoystick;
     private Joystick m_leftJoystick;
 
     public DriveTrainSubsystem(Joystick p_rightJoystick, Joystick p_leftJoystick)
     {
-        m_leftmotors.setInverted(true);
+        m_leftMotors.setInverted(true);
         m_rightJoystick = p_rightJoystick;
         m_leftJoystick= p_leftJoystick;
         
@@ -28,8 +28,8 @@ public class DriveTrainSubsystem extends SubsystemBase {
     @Override
     public void periodic()
     {
-        m_rightmotors.set(m_rightJoystick.getY());
-        m_leftmotors.set(m_leftJoystick.getY());
+        m_rightMotors.set(m_rightJoystick.getY());
+        m_leftMotors.set(m_leftJoystick.getY());
     }
 }
 
