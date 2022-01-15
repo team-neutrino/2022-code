7880f52181bc775df94bf08b14ea8b7a50728ece
Neutrino Controls <NeutrinoControls@gmail.com>
2022-01-15 13:39:20 -0800

    swag

--- 2022-code/src/main/java/frc/robot/RobotContainer.java
+++ 2022-code/src/main/java/frc/robot/RobotContainer.java
@@ -32,7 +32,6 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    
     // Configure the button bindings
     configureButtonBindings();
     
