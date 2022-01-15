43b6a5634579a80fea64202159b26f25038227fa
Neutrino Controls <NeutrinoControls@gmail.com>
2022-01-15 13:38:08 -0800

    SWAG

--- 2022-code/src/main/java/frc/robot/Constants.java
+++ 2022-code/src/main/java/frc/robot/Constants.java
@@ -3,7 +3,6 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-import com.revrobotics.CANSparkMax;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
