bd7018ac1d13c450f61bab5f5e6c8be64e927763
CaleWineinger <70918360+CaleWineinger@users.noreply.github.com>
2022-01-19 15:41:38 -0600

    getting rid of stuff

--- 2022-code/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ 2022-code/src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -5,7 +5,6 @@ import frc.robot.Constants.CanId;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class DriveTrainSubsystem extends SubsystemBase {
@@ -15,16 +14,10 @@ public class DriveTrainSubsystem extends SubsystemBase {
     private CANSparkMax m_leftMotor2 = new CANSparkMax(CanId.MOTOR_CONTROLLER_DRIVER_RIGHT2, MotorType.kBrushless);
     private MotorControllerGroup m_rightMotors = new MotorControllerGroup(m_rightMotor1, m_rightMotor2);
     private MotorControllerGroup m_leftMotors = new MotorControllerGroup(m_leftMotor1, m_leftMotor2);
-    
-    private Joystick m_rightJoystick;
-    private Joystick m_leftJoystick;
 
-
-    public DriveTrainSubsystem(Joystick p_rightJoystick, Joystick p_leftJoystick)
+    public DriveTrainSubsystem()
     {
         m_leftMotors.setInverted(true);
-        m_leftJoystick = p_leftJoystick;
-        m_rightJoystick = p_rightJoystick;
     }
 
     @Override
@@ -35,14 +28,6 @@ public class DriveTrainSubsystem extends SubsystemBase {
     {
         m_leftMotors.set(m_setLeftSpeed);
         m_rightMotors.set(m_setRightSpeed);
-    }
-    public double leftJoystickPosition() 
-    {
-        return m_leftJoystick.getY();
-    }
-    public double rightJoystickPosistion() 
-    {
-        return m_rightJoystick.getY();
     }
 }
 
