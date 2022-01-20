155c41fc7b6f2bb2cc8daeadef3b420cde141576
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2022-01-19 19:18:34 -0600

    use sparkmax pidcontroller

--- 2022-code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2022-code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -11,6 +11,8 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
@@ -24,10 +26,10 @@ import frc.robot.Constants.Shooter;
 { "all" })
 public class ShooterSubsystem extends SubsystemBase
 {
-    private TalonSRXConfiguration wheelMasterConfig = new TalonSRXConfiguration();
-    private TalonSRXConfiguration wheelFollowerConfig = new TalonSRXConfiguration();
     private CANSparkMax m_wheelMotor;
     private CANSparkMax m_wheelMotor2;
+    private RelativeEncoder m_encoder;
+    private SparkMaxPIDController m_pidController;
   
     private double m_targetVelocity;
 
@@ -37,19 +39,18 @@ public class ShooterSubsystem extends SubsystemBase
 
     public ShooterSubsystem()
     {
-        
-        wheelMasterConfig.slot0.kP = Shooter.WHEEL_P;
-        wheelMasterConfig.slot0.kI = Shooter.WHEEL_I;
-        wheelMasterConfig.slot0.kD = Shooter.WHEEL_D;
-        wheelMasterConfig.slot0.kF = Shooter.WHEEL_F;
-
         m_wheelMotor = new CANSparkMax(Constants.CanId.MOTOR_CONTROLLER_SHOOTER1, MotorType.kBrushless);
         m_wheelMotor2 = new CANSparkMax(Constants.CanId.MOTOR_CONTROLLER_SHOOTER2, MotorType.kBrushless);
+        m_wheelMotor2.follow(m_wheelMotor);
+
+        m_encoder = m_wheelMotor.getEncoder();
+        m_pidController.setFeedbackDevice(m_encoder);
+        m_pidController.setP(Shooter.WHEEL_P);
+        m_pidController.setI(Shooter.WHEEL_I);
+        m_pidController.setD(Shooter.WHEEL_D);
+        
         m_wheelMotor.restoreFactoryDefaults();
         m_wheelMotor2.restoreFactoryDefaults();
-       
-        m_wheelMotor2.follow(m_wheelMotor);
-       
 
         m_wheelMotor.setInverted(true);
         m_wheelMotor2.setInverted(true);
@@ -57,6 +58,7 @@ public class ShooterSubsystem extends SubsystemBase
         m_wheelMotor.setIdleMode(IdleMode.kBrake);
         m_wheelMotor2.setIdleMode(IdleMode.kBrake);
        
+
     }
 
     @Override
