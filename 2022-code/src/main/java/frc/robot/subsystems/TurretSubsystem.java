c6a662351d4296c530d3b19d4b199dc9ffdf7eb0
gurellia53 <94152190+daviddong23@users.noreply.github.com>
2022-01-24 20:00:22 -0600

    changes, method for conversion

--- 2022-code/src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ 2022-code/src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -13,14 +13,20 @@ import frc.robot.Constants;
 
 public class TurretSubsystem extends SubsystemBase {
 
-  double m_initialAngle;
-  double m_currentAngle;
+  private double m_initialAngle;
+  private double m_currentAngle;
+  private final double m_minAngle = 5;
+  private final double m_maxAngle = -90;
 
   private TalonSRX m_turretMotor = new TalonSRX(Constants.CANIDConstants.TURRET_MOTOR_ID);
   /** Creates a new TurretSubsystem. */
   public TurretSubsystem() {
     // 3.88 is gear ratio conversion for 2021 robot. Probably delete once code is moved to new robot. 
-    m_initialAngle = ((360.0/1024.0) * m_turretMotor.getSelectedSensorPosition()) / 3.88;
+    m_initialAngle = angleConversion(m_turretMotor.getSelectedSensorPosition());
+  }
+
+  public double angleConversion(double sensorPosition){
+    return (sensorPosition * (360.0/1024.0)) / 3.88;
   }
 
   public void stop(){
@@ -28,7 +34,7 @@ public class TurretSubsystem extends SubsystemBase {
   }
 
   public void turnClockwise(){
-    if (getCurrentAngle() >= 5){
+    if (getCurrentAngle() >= m_minAngle){
       stop(); 
     }
     else {
@@ -37,7 +43,7 @@ public class TurretSubsystem extends SubsystemBase {
   }
 
   public void turnCounterClockwise(){
-    if (getCurrentAngle() <= -90){
+    if (getCurrentAngle() <= m_maxAngle){
       stop();
     }
     else {
@@ -51,6 +57,6 @@ public class TurretSubsystem extends SubsystemBase {
  
   @Override
   public void periodic() {                 
-    m_currentAngle = ((360.0/1024.0) * m_turretMotor.getSelectedSensorPosition()) / 3.88;
+    m_currentAngle = angleConversion(m_turretMotor.getSelectedSensorPosition());
   }
 }
