4df8bc7bc9914d2d99731cc0b276035788937d50
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2022-01-26 17:56:34 -0600

    Update Constants.java

--- 2022-code/src/main/java/frc/robot/Constants.java
+++ 2022-code/src/main/java/frc/robot/Constants.java
@@ -30,6 +30,6 @@ public final class Constants {
         public static final int MOTOR_CONTROLLER_INTAKE_FEED = 5;
         public static final int TURRET_MOTOR_ID = 7;
         public static final int SHOOTER_MOTOR_1_ID = 8;
-        public static final int SHOOTER_MOTOR_2_ID =9;
+        public static final int SHOOTER_MOTOR_2_ID = 9;
     }
 }
