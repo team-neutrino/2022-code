1efe32c6dd1cdfe43c8c2e35e3a43dceffbd4210
Neutrino Controls <885551win@ames.k12.ia.us>
2022-02-02 15:57:05 -0800

    ayayaya

--- 2022-code/src/main/java/frc/robot/commands/ClimbKeylockCommand.java
+++ 2022-code/src/main/java/frc/robot/commands/ClimbKeylockCommand.java
@@ -2,7 +2,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ClimberSubsystem;
 
-public class ClimbKeyLockCommand extends CommandBase{
+public class ClimbKeylockCommand extends CommandBase{
     private ClimberSubsystem m_climberSubsystem;
 
     public ClimbKeyLockCommand (ClimberSubsystem subsystem)
