daaa4b86d2441a8e8087893af682369c3e6f793f
Neutrino Controls <885551win@ames.k12.ia.us>
2022-02-12 16:03:43 -0600

    shooter tuning

--- 2022-code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2022-code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -20,7 +20,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
   private final double WHEEL_I = 0;
   private final double WHEEL_D = 0;
-  private final double WHEEL_FF = 0;
+  private final double WHEEL_FF = 0.15;
 
   private CANSparkMax m_wheelMotor;
   private CANSparkMax m_wheelMotor2;
@@ -48,14 +48,16 @@ public class ShooterSubsystem extends SubsystemBase {
     m_wheelMotor.setIdleMode(IdleMode.kCoast);
     m_wheelMotor2.setIdleMode(IdleMode.kCoast);
 
+    m_wheelMotor.setClosedLoopRampRate(2);
+
     m_encoder1 = m_wheelMotor.getEncoder();
     m_encoder2 = m_wheelMotor2.getEncoder();
     m_pidController = m_wheelMotor.getPIDController();
     m_pidController.setFeedbackDevice(m_encoder1);
-    m_pidController.setP(WHEEL_P);
-    m_pidController.setI(WHEEL_I);
-    m_pidController.setD(WHEEL_D);
-    m_pidController.setFF(WHEEL_FF);
+    m_pidController.setP(WHEEL_P / 1000);
+    m_pidController.setI(WHEEL_I / 1000);
+    m_pidController.setD(WHEEL_D / 1000);
+    m_pidController.setFF(WHEEL_FF / 1000);
     m_pidController.setOutputRange(.1, 1);
   }
 
@@ -92,34 +94,34 @@ public class ShooterSubsystem extends SubsystemBase {
   }
 
   public double getP() {
-    return m_pidController.getP();
+    return m_pidController.getP() * 1000;
   }
 
   public double getFF() {
-    return m_pidController.getFF();
+    return m_pidController.getFF() * 1000;
   }
 
   public void setP(double P) {
-    m_pidController.setP(P);
+    m_pidController.setP(P / 1000);
   }
 
   public double getI() {
-    return m_pidController.getI();
+    return m_pidController.getI() * 1000;
   }
 
   public void setI(double I) {
-    m_pidController.setI(I);
+    m_pidController.setI(I / 1000);
   }
 
   public double getD() {
-    return m_pidController.getD();
+    return m_pidController.getD() * 1000;
   }
 
   public void setD(double D) {
-    m_pidController.setD(D);
+    m_pidController.setD(D / 1000);
   }
 
   public void setFF(double FF) {
-    m_pidController.setFF(FF);
+    m_pidController.setFF(FF / 1000);
   }
 }
