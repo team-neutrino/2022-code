7ecca78b8536570f69314bf34d7eab2d734445c1
davidd23 <77417861+ddavidd23@users.noreply.github.com>
2022-03-02 19:38:14 -0600

    a

--- 2022-code/src/main/java/frc/robot/TrajectoryConfigConstants.java
+++ 2022-code/src/main/java/frc/robot/TrajectoryConfigConstants.java
@@ -11,8 +11,8 @@ public class TrajectoryConfigConstants {
   public static final double K_TRACK_WIDTH_METERS = 0.7;
   public static final DifferentialDriveKinematics K_DRIVE_KINEMATICS =
       new DifferentialDriveKinematics(K_TRACK_WIDTH_METERS);
-  public static final double K_MAX_SPEED_METERS_PER_SECOND = 1;
-  public static final double K_HALF_SPEED_METERS_PER_SECOND = .5;
+  public static final double K_MAX_SPEED_METERS_PER_SECOND = .5;
+  public static final double K_HALF_SPEED_METERS_PER_SECOND = .25;
   public static final double K_MAX_ACCELERATION_METERS_PER_SECOND_SQUARED = 6.8;
   public static final double K_RAMSETE_BETA = 2;
   public static final double K_RAMSETE_ZETA = 0.7;
