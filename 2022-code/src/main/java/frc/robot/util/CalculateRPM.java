48bd4431f64093a0d03aaae018d13273ae5a8105
CaleWineinger <70918360+CaleWineinger@users.noreply.github.com>
2022-03-05 16:12:27 -0600

    tuning

--- 2022-code/src/main/java/frc/robot/util/CalculateRPM.java
+++ 2022-code/src/main/java/frc/robot/util/CalculateRPM.java
@@ -18,20 +18,9 @@ public class CalculateRPM {
 
     m_limelight = p_limelight;
 
-    m_distanceRPMData.put(1.1, 2650.0);
-    m_distanceRPMData.put(1.3, 2800.0);
-    m_distanceRPMData.put(1.52, 2900.0);
-    m_distanceRPMData.put(1.78, 2950.0);
-    m_distanceRPMData.put(2.0, 3300.0);
-    m_distanceRPMData.put(2.5, 3650.0);
-    m_distanceRPMData.put(2.8, 3800.0);
-    m_distanceRPMData.put(3.0, 3900.0);
-    m_distanceRPMData.put(3.5, 4200.0);
-    m_distanceRPMData.put(3.8, 4350.0);
-    m_distanceRPMData.put(4.0, 4500.0);
-    m_distanceRPMData.put(4.2, 4700.0);
-    m_distanceRPMData.put(4.66, 5350.0);
-    m_distanceRPMData.put(4.9, 5350.0);
+    m_distanceRPMData.put(1.09, 2100.0);
+    m_distanceRPMData.put(2.9, 3000.0);
+    m_distanceRPMData.put(4.6, 5000.0);
   }
 
   public double InterpolateDistance() {
