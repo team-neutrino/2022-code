a92d5171b6b1752409d03fa69d96ee530935dffb
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2022-03-11 18:31:30 -0600

    spotlessJavaApply

--- 2022-code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ 2022-code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -17,6 +17,7 @@ import frc.robot.util.CalculateRPM;
 public class ShooterSubsystem extends SubsystemBase {
   /** Shooter Constants */
   private final double WHEEL_P = 0.5;
+
   private final double WHEEL_I = 0.3;
   private final double WHEEL_D = 0;
   private final double WHEEL_FF = 0.2;
