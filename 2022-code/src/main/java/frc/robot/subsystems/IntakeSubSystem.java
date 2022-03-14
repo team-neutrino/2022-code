cdbdb78281e3f837ac22026a3f1ac271a9703a21
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2022-03-14 18:27:41 -0500

    create pressure sensor

--- 2022-code/src/main/java/frc/robot/subsystems/IntakeSubSystem.java
+++ 2022-code/src/main/java/frc/robot/subsystems/IntakeSubSystem.java
@@ -6,27 +6,35 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.AnalogOutput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CANIDConstants;
+import frc.robot.Constants.DigitalConstants;
 
 public class IntakeSubSystem extends SubsystemBase {
-  /** Intake Constants */
   private final int SOLENOID_INTAKE_FORWARD = 4;
-
   private final int SOLENOID_INTAKE_REVERSE = 5;
   private final double INTAKE_MOTOR_POWER = 1;
+  private final double PRESSURE_SENSOR_INPUT_VOLTAGE = 5.0;
 
   private CANSparkMax m_IntakeFeedMotor =
       new CANSparkMax(CANIDConstants.MOTOR_CONTROLLER_INTAKE_FEED, MotorType.kBrushless);
   private DoubleSolenoid m_IntakeSolenoid =
       new DoubleSolenoid(
           PneumaticsModuleType.CTREPCM, SOLENOID_INTAKE_FORWARD, SOLENOID_INTAKE_REVERSE);
+  private AnalogOutput m_PressureSensor = new AnalogOutput(DigitalConstants.PRESSURE_SENSOR);
 
   public IntakeSubSystem() {
     m_IntakeFeedMotor.setOpenLoopRampRate(.5);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
   }
 
   public void setDown() {
@@ -49,8 +57,13 @@ public class IntakeSubSystem extends SubsystemBase {
     m_IntakeFeedMotor.set(0);
   }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+  private double getPressureSensorOutputVoltage()
+  {
+    return m_PressureSensor.getVoltage();
+  }
+
+  public double getPressure()
+  {
+    return 250*(getPressureSensorOutputVoltage()/PRESSURE_SENSOR_INPUT_VOLTAGE)-25;
   }
 }
