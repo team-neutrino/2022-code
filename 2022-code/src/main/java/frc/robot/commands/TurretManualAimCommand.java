5ef3a2e423db236356f57261436d537cbbb32e4e
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2022-03-23 22:51:41 -0500

    reorganized

--- 2022-code/src/main/java/frc/robot/commands/TurretManualAimCommand.java
+++ 2022-code/src/main/java/frc/robot/commands/TurretManualAimCommand.java
@@ -23,7 +23,9 @@ public class TurretManualAimCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
