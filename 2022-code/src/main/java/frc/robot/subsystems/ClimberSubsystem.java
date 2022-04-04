7d7f263b5c6eb96149e8248843963018de17bd3d
Andrew <714.leslie.kim@gmail.com>
2022-04-04 18:10:15 -0500

    poggers

--- 2022-code/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ 2022-code/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -30,8 +30,10 @@ public class ClimberSubsystem extends SubsystemBase {
   public ClimberSubsystem() {
     m_climber.setIdleMode(IdleMode.kBrake);
     m_climber.setInverted(true);
+    m_climber.setSmartCurrentLimit(40);
     m_climber.setOpenLoopRampRate(.5);
     m_encoder = m_climber.getEncoder();
+    m_climber.burnFlash();
   }
 
   public void keyLock() {
