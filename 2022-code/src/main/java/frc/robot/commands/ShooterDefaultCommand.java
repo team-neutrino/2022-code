397e98936fc95c106da9d72926919cb0cfe8ef1f
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2022-04-12 18:52:58 -0500

    changing random constants

--- 2022-code/src/main/java/frc/robot/commands/ShooterDefaultCommand.java
+++ 2022-code/src/main/java/frc/robot/commands/ShooterDefaultCommand.java
@@ -30,7 +30,7 @@ public class ShooterDefaultCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_shooter.setTargetRPM(1400);
+    m_shooter.setTargetRPM(2000);
     // m_shooter.turnOff();
   }
 
