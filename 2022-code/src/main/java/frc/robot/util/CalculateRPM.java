e7690b5907e84a8a454b7f3f2967c19bf82ca354
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2022-09-13 18:54:36 -0500

    return

--- 2022-code/src/main/java/frc/robot/util/CalculateRPM.java
+++ 2022-code/src/main/java/frc/robot/util/CalculateRPM.java
@@ -6,10 +6,49 @@ package frc.robot.util;
 
 import frc.robot.subsystems.LimelightSubsystem;
 import java.util.*;
-wqegjqygeyjqw
+
 /** Add your docs here. */
 public class CalculateRPM {
 
+  TreeMap<Double, Double> m_distanceRPMData = new TreeMap<Double, Double>();
+
+  LimelightSubsystem m_limelight;
+  public static final double K_MIN_CALCULABLE = 1.78;
+  public static final double K_MAX_CALCULABLE = 6.05;
+
+  public CalculateRPM(LimelightSubsystem p_limelight) {
+
+    m_limelight = p_limelight;
+
+    m_distanceRPMData.put(1.78, 2770.0);
+    m_distanceRPMData.put(2.16, 2770.0);
+    m_distanceRPMData.put(2.54, 3070.0);
+    // add something between
+    m_distanceRPMData.put(3.69, 3270.0);
+    m_distanceRPMData.put(4.2, 3390.0);
+    m_distanceRPMData.put(6.05, 3870.0);
+  }
+
+  public double InterpolateDistance() {
+
+    double smallerDistance = 0.0;
+    double largerDistance = 0.0;
+    double rpm = 0.0;
+
+    double limeLightDistance = m_limelight.getDistance();
+
+    if (limeLightDistance <= m_distanceRPMData.firstKey()) {
+      return m_distanceRPMData.get(m_distanceRPMData.firstKey());
+    } else if (limeLightDistance >= m_distanceRPMData.lastKey()) {
+      return m_distanceRPMData.get(m_distanceRPMData.lastKey());
+    } else {
+      for (Double a : m_distanceRPMData.keySet()) {
+        if (a >= limeLightDistance) {
+          largerDistance = a;
+          break;
+        } else {
+          smallerDistance = a;
+        }
       }
     }
 
