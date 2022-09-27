9f5ca9776996559bc785c14e85f3eb78304e5827
Noah9847 <70857168+Noah9847@users.noreply.github.com>
2022-09-27 10:15:27 -0500

    comments

--- 2022-code/src/main/java/frc/robot/util/ShootWhileMove.java
+++ 2022-code/src/main/java/frc/robot/util/ShootWhileMove.java
@@ -16,6 +16,14 @@ public class ShootWhileMove {
 
     }
 
+    /*conversion needed from encoder counts to some actual unit. Right now, the deltaA is in encoder units and the limelight
+     * values are also apparently being converted by taking degrees and multiplying by 10. the change in angle by the turret
+     * needs to be in radians since that is a unitless measure that actually converts to a distance. If the limelight output
+     * is in degrees that should be easy enough already. This should then be added to or replaced with the input that is set
+     * current angle I think somewhere in a default command. I think I had a question about that that I also wanted to ask?
+     * Look into it. 
+     */
+
     public double feedfowardTanV(){
         double deltaTx = m_limelight.getDeltaTx() * LIMELIGHT_MULTIPLICATION;
         double deltaA = m_turret.getDeltaA();
