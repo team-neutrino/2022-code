156f0b8e83f3804766dcfc9329398e3ac505796e
Neutrino Controls <NeutrinoControls@gmail.com>
2022-10-04 18:16:41 -0500

    spotless

--- 2022-code/src/main/java/frc/robot/commands/TurretToAngleCommand.java
+++ 2022-code/src/main/java/frc/robot/commands/TurretToAngleCommand.java
@@ -28,8 +28,7 @@ public class TurretToAngleCommand extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
