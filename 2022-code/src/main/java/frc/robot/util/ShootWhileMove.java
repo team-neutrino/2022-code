1ff127b7e5172f4a682a4ada788808f832fce9a4
Noah9847 <70857168+Noah9847@users.noreply.github.com>
2022-10-07 17:04:12 -0500

    deleting unneeded comments and cleaning stuff up

--- 2022-code/src/main/java/frc/robot/util/ShootWhileMove.java
+++ 2022-code/src/main/java/frc/robot/util/ShootWhileMove.java
@@ -7,9 +7,8 @@ public class ShootWhileMove {
     
     TurretPIDSubsystem m_turret;
     LimelightSubsystem m_limelight;
-    double LIMELIGHT_MULTIPLICATION = 10.0;
+    // needed?? double LIMELIGHT_MULTIPLICATION = 10.0;
     double TX_TO_TURRET_COUNTS_CONVERSION = 165/240;
-    double cycles = 0;
 
     public ShootWhileMove(TurretPIDSubsystem p_turret, LimelightSubsystem p_limelight){
 
@@ -18,17 +17,7 @@ public class ShootWhileMove {
 
     }
 
-    /*conversion needed from encoder counts to some actual unit. Right now, the deltaA is in encoder units and the limelight
-     * values are also apparently being converted by taking degrees and multiplying by 10. the change in angle by the turret
-     * needs to be in radians since that is a unitless measure that actually converts to a distance. If the limelight output
-     * is in degrees that should be easy enough already. This should then be added to or replaced with the input that is set
-     * current angle I think somewhere in a default command. I think I had a question about that that I also wanted to ask?
-     * Look into it. 
-     */
-
     public double integrateAngularV() {
-        cycles++;
-        if (cycles >= 12){
         double deltaTx = m_limelight.getDeltaTx() * TX_TO_TURRET_COUNTS_CONVERSION;
         double deltaA = m_turret.getDeltaA();
         System.out.println("Delta A: " + deltaA);
@@ -60,10 +49,6 @@ public class ShootWhileMove {
         double integrateV = (vAverage + vAverageTwo) * 0.2; 
         System.out.println(integrateV);
         return integrateV;
-        }
-        else {
-            return 0.0;
-        }
     }
 
 }
